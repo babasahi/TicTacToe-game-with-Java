ac933f61662a1db3e645c7e7e0be6caed0aeadea
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2022-10-26 11:03:36 +0100

    added new game logic resolution

---
+++ Game.java
@@ -0,0 +1,70 @@
+
+enum gameStatus {
+	playing,
+	even,
+	xWin,
+	oWIn,
+}
+
+public class Game {
+
+	private static int[] board = new int[9];
+
+	public static void initBoard() {
+
+		for (int i = 0; i < board.length; i++) {
+			board[i] = 0;
+		}
+	}
+
+	public static void play(int indenum, int play) {
+		if (play != 0) {
+			board[indenum] = play;
+		}
+
+	}
+
+	static gameStatus checkGameStatus() {
+		int playedCount = 0;
+		for (int i = 0; i < board.length; i++) {
+			if (board[i] != 0) {
+				playedCount++;
+			}
+		}
+		if (playedCount < 5) {
+			return gameStatus.playing;
+		} else if (playedCount == 9) {
+			return gameStatus.even;
+		} else {
+			if (lookForWin(-1)) {
+				return gameStatus.oWIn;
+			} else {
+				return gameStatus.xWin;
+			}
+		}
+
+	}
+
+	static boolean lookForWin(int num) {
+		if (board[0] == num && board[1] == num && board[2] == num) {
+			return true;
+		} else if (board[3] == num && board[4] == num && board[5] == num) {
+			return true;
+		} else if (board[6] == num && board[7] == num && board[8] == num) {
+			return true;
+		} else if (board[0] == num && board[3] == num && board[6] == num) {
+			return true;
+		} else if (board[1] == num && board[4] == num && board[7] == num) {
+			return true;
+		} else if (board[2] == num && board[5] == num && board[8] == num) {
+			return true;
+		} else if (board[0] == num && board[4] == num && board[8] == num) {
+			return true;
+		} else if (board[2] == num && board[4] == num && board[6] == num) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+}
