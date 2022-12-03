e84af478fefd1e5faa299e03971e0d2f28473b8d
babe-saleh-mahfoud <babesalehmahfoud@gmail.com>
2022-12-03 23:05:17 +0100

    finished logic

--- socketsgame/GameLogic.java
+++ socketsgame/GameLogic.java
@@ -1,8 +1,37 @@
 package socketsgame;
 
-class GameLogic {
+public class GameLogic {
 
 	public int[] board = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+	public static String numToSymbol(int num) {
+		if (num == -1) {
+			return "O";
+		} else if (num == 1) {
+			return "X";
+		} else {
+			return " ";
+		}
+	}
+
+	public static void printBoard(int[] board) {
+		System.out.println("");
+		System.out.println("");
+		System.out.println(
+				"     " + numToSymbol(board[0]) + "  |  " + numToSymbol(board[1]) + "  | "
+						+ numToSymbol(board[2]));
+		System.out.println("    " + "_______________");
+		System.out.println(
+				"     " + numToSymbol(board[3]) + "  |  " + numToSymbol(board[4]) + "  | "
+						+ numToSymbol(board[5]));
+		System.out.println("    " + "_______________");
+		System.out.println(
+				"     " + numToSymbol(board[6]) + "  |  " + numToSymbol(board[7]) + "  | "
+						+ numToSymbol(board[8]));
+		System.out.println("");
+		System.out.println("");
+
+	}
 
 	public void play(int position, int play) {
 		if (board[position] == 0) {
--- socketsgame/TicTacToe.java
+++ socketsgame/TicTacToe.java
@@ -6,41 +6,12 @@ public class TicTacToe {
 
 	static Scanner scanner = new Scanner(System.in);
 
-	private static void printBoard(int[] board) {
-		System.out.println("");
-		System.out.println("");
-		System.out.println(
-				"     " + numToSymbol(board[0]) + "  |  " + numToSymbol(board[1]) + "  | "
-						+ numToSymbol(board[2]));
-		System.out.println("    " + "_______________");
-		System.out.println(
-				"     " + numToSymbol(board[3]) + "  |  " + numToSymbol(board[4]) + "  | "
-						+ numToSymbol(board[5]));
-		System.out.println("    " + "_______________");
-		System.out.println(
-				"     " + numToSymbol(board[6]) + "  |  " + numToSymbol(board[7]) + "  | "
-						+ numToSymbol(board[8]));
-		System.out.println("");
-		System.out.println("");
-
-	}
-
-	static private String numToSymbol(int num) {
-		if (num == -1) {
-			return "O";
-		} else if (num == 1) {
-			return "X";
-		} else {
-			return " ";
-		}
-	}
-
 	public static void main(String[] args) {
 		GameLogic game = new GameLogic();
 		int turn = 0;
 		int index = 0;
 
-		printBoard(game.board);
+		GameLogic.printBoard(game.board);
 		int firstPlay = 0;
 
 		System.out.println("Start with X :");
